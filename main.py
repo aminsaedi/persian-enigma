d1415d3c4271f70ec679ab11187271db63d4f6fb
Amin Saedi <amin.di.sam@gmail.com>
2022-12-22 23:13:05 -0500

    english version completed

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 total_rotate_count = 0
 
 
+def rotate_rotors():
+    global total_rotate_count
+    total_rotate_count += 1
+    rotors[0] = rotator(rotors[0])
+    if (total_rotate_count % 26 == 0):
+        rotors[1] = rotator(rotors[1])
+    if (total_rotate_count % 52 == 0):
+        rotors[2] = rotator(rotors[2])
+
+
+def rotator(input):
+    output = ""
+    for index in range(len(input) - 1):
+        output += input[index + 1]
+    output += input[0]
+    return output
+
+
 def enigma_for_char(char):
     r0 = alphabet.find(char)
     r1 = rotors[0][r0]
@@ -22,32 +40,40 @@ def enigma_for_char(char):
     r2_back = alphabet[r2_back_index]
     r1_back_index = rotors[0].find(r2_back)
     r1_back = alphabet[r1_back_index]
-
-    global total_rotate_count
-    total_rotate_count += 1
-    rotors[0] = rotator(rotors[0])
-    if (total_rotate_count % 26 == 0):
-        rotors[1] = rotator(rotors[1])
-    if (total_rotate_count % 52 == 0):
-        rotors[2] = rotator(rotors[2])
-
+    rotate_rotors()
     return r1_back
 
 
-def rotator(input):
-    output = ""
-    for index in range(len(input) - 1):
-        output += input[index + 1]
-    output += input[0]
-    return output
+def decode_char(char):
+    r1 = rotors[0][alphabet.find(char)]
+    r2 = rotors[1][alphabet.find(r1)]
+    r3 = rotors[2][alphabet.find(r2)]
+    reflected = reflector(r3)
+    r3_rev = alphabet[rotors[2].find(reflected)]
+    r2_rev = alphabet[rotors[1].find(r3_rev)]
+    r1_rev = alphabet[rotors[0].find(r2_rev)]
+    rotate_rotors()
+    return r1_rev
 
-
-
-plain = "hihihihi"
+plain = "hihihihimynameisaminsaediimlivinginmontrealcanadaimwebdevelopernowimcookingfoodformyselfandimaloneallthetimes"
 cipher = ""
 
-for char in plain:
-    cipher += enigma_for_char(char)
+# for char in plain:
+#    cipher += enigma_for_char(char)
 
 print("Plain: " + plain)
 print("Cipher: " + cipher)
+
+ciphered = "nhgazcbvwemsyfjyludvlfzyqoxwcntjrptsgfnxahfdwjzvtcgcikzhomdpggvveehqjkgtrjpofqaujthvyjgkntatpxcsrrcagolbnrnyn"
+
+
+total_rotate_count = 0
+decoded = ""
+for char in ciphered:
+    decoded += decode_char(char)
+
+print("Decoded: ", decoded)
+
+
+
+
